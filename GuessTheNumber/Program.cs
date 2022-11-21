ecd1f8df189e3c2c09ee3faf4805677131a6aaa9
antonia-manolache <anto.manolache@gmail.com>
2022-11-21 10:21:56 +0200

    We separate the ReadValue and ValidateRangeLimitValues methods.

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,23 +1,33 @@
 ﻿int rangeStart;
+int rangeEnd=0;
 
-static bool ReadIntValue(out int rangeStart, string intValueDescription)
+static bool ValidateRangeLimitValues(int rangeStart, int rangeEnd)
 {
-    Console.WriteLine(intValueDescription);
-    string i = Console.ReadLine();
-    return Int32.TryParse(i, out rangeStart);
+    return rangeStart >= rangeEnd;
 }
 
-int rangeEnd;
+
+
+string ReadValue(string valueDescription)
+{
+    Console.WriteLine(valueDescription);
+    return Console.ReadLine();
+}
+
+
+
 bool isRangeValid;
 do
 {
     isRangeValid = true;
 
-    var ok1=ReadIntValue(out rangeStart, "Inceputul intervalului:");
+    var rangeStartString = ReadValue("Inceputul intervalului:");
 
-    var ok2 = ReadIntValue(out rangeEnd, "Sfarsitul intervalului:");
+    var rangeEndString = ReadValue("Sfarsitul intervalului:");
 
-    if (rangeStart >= rangeEnd || ok1 == false || ok2 == false)
+    if (!int.TryParse(rangeStartString,out rangeStart)
+       || !int.TryParse(rangeEndString, out rangeEnd)
+       || !ValidateRangeLimitValues(rangeStart, rangeEnd))
     {
         isRangeValid = false;
         Console.WriteLine("Interval introdus gresit! Incearca din nou.");
@@ -33,7 +43,7 @@ do
 {
     string u = Console.ReadLine();
     int user;
-    var usr=Int32.TryParse(u, out user);
+    var usr = Int32.TryParse(u, out user);
 
     if (usr == false)
     {
@@ -41,17 +51,17 @@ do
         continue;
     }
     else
-    if (user > rangeEnd || user < rangeStart && usr==false)
-        Console.WriteLine("Valoarea "+user+" nu se afla in interval!");
+    if (user > rangeEnd || user < rangeStart && usr == false)
+        Console.WriteLine("Valoarea " + user + " nu se afla in interval!");
     else
         if (nr == user)
-        {
-            guess = true;
-            Console.WriteLine("Valoarea " + user + " este corect!");
-        }
-        else
+    {
+        guess = true;
+        Console.WriteLine("Valoarea " + user + " este corect!");
+    }
+    else
             if (user > nr)
-                Console.WriteLine("Valoarea " + user + " este prea mare!");
-            else
-                Console.WriteLine("Valoarea " + user + " este prea mic!");
+        Console.WriteLine("Valoarea " + user + " este prea mare!");
+    else
+        Console.WriteLine("Valoarea " + user + " este prea mic!");
 } while (guess == false);
